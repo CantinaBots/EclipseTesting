f62e4e085b1a7d338b3ff6c28a6b19ce53037b0a
connor <connor@localhost>
2021-05-22 01:09:53 -0500

    Helllo from mac.

--- TestingProject/src/TestingPackage/HelloWorld.java
+++ TestingProject/src/TestingPackage/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	System.out.println("Hello World.");
 	System.out.println("About to try on mac.");
 	System.out.println("see if this works.");
+	System.out.println("Testing from mac.");
 	}
 	
 }
