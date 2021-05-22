3d341e903ae6eaa0eaf014f884dd79269e70088f
CantinaBots <cthaxton@cantinacapital.com>
2021-05-22 01:10:50 -0500

    Mac test 1

--- TestingProject/src/TestingPackage/HelloWorld.java
+++ TestingProject/src/TestingPackage/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String args[]) {
 	System.out.println("Hello World.");
 	System.out.println("About to try on mac.");
+	System.out.println("see if this works.");
 	}
 	
 }
