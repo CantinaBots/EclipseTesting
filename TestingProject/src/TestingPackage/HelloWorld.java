e1bbc4fad315f4d1d96110808f9b8f22e34c5877
CantinaBots <cthaxton@cantinacapital.com>
2021-05-22 01:05:55 -0500

    mac next...

--- TestingProject/src/TestingPackage/HelloWorld.java
+++ TestingProject/src/TestingPackage/HelloWorld.java
@@ -3,6 +3,7 @@ package TestingPackage;
 public class HelloWorld {
 	public static void main(String args[]) {
 	System.out.println("Hello World.");
+	System.out.println("About to try on mac.");
 	}
 	
 }
